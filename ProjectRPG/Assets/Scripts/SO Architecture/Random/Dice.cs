83c877492bf898d331ce85e2385d8d73f9ab1d56
Garbriel Zimmermann <garbriel.zimmermann98@gmail.com>
2019-09-04 14:39:10 +0200

    [General] Dice So added
    Includes Editor script

--- ProjectRPG/Assets/Scripts/SO Architecture/Random/Dice.cs
+++ ProjectRPG/Assets/Scripts/SO Architecture/Random/Dice.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using SOArchitecture.Reference;
 
-public class Dice : ScriptableObject{
-	public int[] numbers;
-	public int currentRoll;
+namespace SOArchitecture.Random{
+	[CreateAssetMenu(fileName = "New Dice", menuName = "Dice")]
+	public class Dice : ScriptableObject{
+		[Tooltip("First value of the dice.")]
+		public int minValue = 1;
+		[Tooltip("Last value of the dice.")]
+		public int maxValue = 6;
 
-	public void Roll(){
-		Dictionary<int, bool> dictionary = new Dictionary<int, bool>(numbers.Length);
-		foreach(var number in numbers){
-			bool isHit = Random.value > 0.5f;
-			dictionary.Add(number, isHit);
-			// thats wrong
+		private int currentRoll;
+		public int CurrentRoll{
+			get{
+				return currentRoll;
+			}
+		}
+
+		private int numberOfFaces = 6;
+		public int NumberOfFaces{
+			get{
+				return numberOfFaces;
+			}
+		}
+
+		public void Roll(){
+			currentRoll = UnityEngine.Random.Range(minValue, maxValue);
+		}
+
+		public void RecalculateNumberOfFaces(){
+			numberOfFaces = maxValue - minValue + 1;
 		}
 	}
-
-	public int GetCurrentDiceRoll(){
-		return currentRoll;
-	}
 }
+
---
+++ ProjectRPG/Assets/Scripts/SO Architecture/Random/Editor/DiceEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace SOArchitecture.Random.Editors{
+	[CustomEditor(typeof(Dice))]
+	public class DiceEditor : Editor{
+		private SerializedObject obj;
+		private SerializedProperty currentRoll;
+		private SerializedProperty numberOfFaces;
+
+		public void OnEnable(){
+			obj = new SerializedObject(target);
+			currentRoll = serializedObject.FindProperty("currentRoll");
+			numberOfFaces = serializedObject.FindProperty("numberOfFaces");
+		}
+
+		public override void OnInspectorGUI(){
+			obj.Update();
+
+			base.OnInspectorGUI();
+			
+			Dice e = target as Dice;
+
+			if(GUI.changed){
+				e.RecalculateNumberOfFaces();
+			}
+			
+			GUI.enabled = false;
+
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.IntField(new GUIContent("Current Roll", "Call Roll() to randomize a new number between startValue and endValue (both inclusive)."), e.CurrentRoll);
+			EditorGUILayout.EndHorizontal();
+
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.IntField(new GUIContent("Number of Faces", "Current number of faces the dice has. Dice usually have an even number of faces."), e.NumberOfFaces);
+			EditorGUILayout.EndHorizontal();
+
+			GUI.enabled = true;
+
+			GUI.enabled = Application.isPlaying;
+			if (GUILayout.Button("Roll")){
+				e.Roll();
+			}
+
+			obj.ApplyModifiedProperties();    
+		}
+	}
+}
